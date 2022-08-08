2908c00e4963794323117f2175ddc5fcfb2cc09c
Rafabass22 <rafaelpereiradasilva.pereira2@gmail.com>
2022-08-08 17:09:18 -0300

    Mini Projetos

---
+++ Calculadora/scripts.js
@@ -0,0 +1,5 @@
+const previousOperationText = document.querySelector("#previous-operation")
+const currentOperationText = document.querySelector("#current-operation")
+const buttons = document.querySelectorAll("#buttons-container button")
+
+console.log(buttons);
